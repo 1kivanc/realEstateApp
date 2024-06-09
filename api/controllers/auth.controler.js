b2b2e1df242baf6a01d6fc7891a6b440227120cf
1kivanc <kivanc0556@gmail.com>
2024-06-09 09:34:13 +0300

    login and register page

--- api/controllers/auth.controler.js
+++ api/controllers/auth.controler.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 export const register = async (req,res) => {
     const {username,email,password} = req.body;
@@ -33,17 +34,24 @@ export const login = async (req,res) => {
         if(!isPasswordValid) return res.status(401).json({message:"Invalid Credentials!"}) 
         
         const age = 1000 * 60 * 60 * 24 * 7
+        const token = jwt.sign({
+            id:user.id
+        },process.env.JWT_SECRET_KEY,{
+            expiresIn:age
+        })
+
+        const {password:userPassword, ...userInfo} = user
         // res.setHeader("Set-Cookie","test="+"myValue").json("success")
-        res.cookie("test2","myValue",{
+        res.cookie("token",token,{
             httpOnly:true,
             // secure:true,
             maxAge:age
-        }).status(200).json({message:"login Successfull"})
+        }).status(200).json(userInfo)
     }catch(err){
         console.log(err);
         res.status(500).json({message:"Falied to login!"})
     }
 }
 export const logout = (req,res) => {
-    
+    res.clearCookie("token").status(200).json({message:"Logout Successfull"});
 }
