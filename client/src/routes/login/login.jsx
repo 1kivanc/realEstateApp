89ef66662bc535c0d9b064e7e38f29fcbc971a02
1kivanc <kivanc0556@gmail.com>
2024-06-15 15:44:17 +0300

    react context auth

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,42 +1,41 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./login.scss";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import apiRequest from "../../lib/apiRequest";
+import { AuthContext } from "../../context/AuthContext";
 
 function Login() {
-  const [error,setError] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const navigate = useNavigate()
+  const {updateUser} = useContext(AuthContext)
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     setIsLoading(true);
     setError("");
     const formData = new FormData(e.target);
-    const username = formData.get("username")
-    const password = formData.get("password")
 
-    try{
-      const res = await apiRequest.post("/auth/login",{
-        username,password
-      })
-      
-      localStorage.setItem("user",JSON.stringify(res.data));
+    const username = formData.get("username");
+    const password = formData.get("password");
 
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        username,
+        password,
+      });
 
-      navigate("/")
+      updateUser(res.data)
 
-    }catch(err){
-        
-        setError(err.response.data.message)
-    }finally{
-      setIsLoading(false)
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
     }
-
-    
-  }
+};
   return (
     <div className="login">
       <div className="formContainer">
