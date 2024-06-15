e357a0b02b158536ca22f2140ca161afc3ee06f9
1kivanc <kivanc0556@gmail.com>
2024-06-15 23:55:09 +0300

    prisma crud

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
-
+import userRoute from "./routes/user.route.js";
 const app = express();
 
 app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
@@ -17,6 +17,7 @@ app.use(cookieParser())
 app.use("/api/post",postRoute);
 app.use("/api/auth",authRoute);
 app.use("/api/test",testRoute);
+app.use("/api/users",userRoute);
 
 
 app.listen(5000,() => {
